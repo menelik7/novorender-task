8ac8ffe212ba420c09bf529c85c843c0d81bbab3
menelik7 <menelikworku@gmail.com>
2023-09-18 10:49:23 +0200

    Refactoring and cleaning up code a little

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,74 +1,75 @@
 import React, { MouseEvent, useState } from "react";
 import Button from "./Button";
 import { ButtonClass } from "./ButtonClass";
+import { ReadonlyVec3, ReadonlyQuat } from "gl-matrix";
+import { FlightController } from "@novorender/api";
 
 interface PositionButton {
 	id: string;
 	label: string;
+	storePosition: (positionArgs: PositionArgs) => void;
+	// moveToPosition: (positionArgs: PositionArgs) => void;
 }
 
-const positionButtons: PositionButton[] = [
-	{
-		id: "001",
-		label: "Position 1",
-	},
-	{
-		id: "002",
-		label: "Position 2",
-	},
-	{
-		id: "003",
-		label: "Position 3",
-	},
-];
+interface PositionArgs {
+	position?: ReadonlyVec3;
+	rotation?: ReadonlyQuat;
+}
 
 export default function Header() {
-	const [firstPosition, setFirstPosition] = useState();
-	const [secondPosition, setSecondPosition] = useState();
-	const [thirdPosition, setThirdPosition] = useState();
+	const [firstPosition, setFirstPosition] = useState<PositionArgs>({});
+	const [secondPosition, setSecondPosition] = useState<PositionArgs>({});
+	const [thirdPosition, setThirdPosition] = useState<PositionArgs>({});
+
+	const positionButtons: PositionButton[] = [
+		{
+			id: "001",
+			label: "Position 1",
+			storePosition: setFirstPosition,
+			// TODO
+			// moveToPosition:
+		},
+		{
+			id: "002",
+			label: "Position 2",
+			storePosition: setSecondPosition,
+			// TODO
+			// moveToPosition:
+		},
+		{
+			id: "003",
+			label: "Position 3",
+			storePosition: setThirdPosition,
+			// TODO
+			// moveToPosition:
+		},
+	];
 
 	const handleClick = (event: MouseEvent) => {
 		const target = event.target as HTMLElement;
-		switch (target.innerText) {
-			case "Position 1":
+
+		// Store position values
+		positionButtons.forEach((positionButton, i) => {
+			if (positionButton.label === target.innerText) {
 				if (event.shiftKey) {
+					console.log("Storing", positionButtons[i].label);
 					// TODO
-					// Store position and rotation
-					// setFirstPosition(position, rotation)
-					console.log("Store position one");
-					break;
+					// Extract position and rotation RenderState??
+					// Create an object with obtained values
+					// const currentPosition: PositionArgs = {
+					// 	position: [x, y, z],
+					// 	rotation: [a, b, c, d]
+					// }
+					//  positionButtons[i].storePosition(position)
+
+					return;
 				}
+
+				console.log("Moving to", positionButtons[i].label);
 				// TODO
-				// Move to stored position
-				console.log("Fly to position one.");
-				break;
-			case "Position 2":
-				if (event.shiftKey) {
-					// TODO
-					// Store position and rotation
-					// setSecondPosition(position, rotation)
-					console.log("Store position two");
-					break;
-				}
-				// TODO
-				// Move to stored position
-				console.log("Fly to position two.");
-				break;
-			case "Position 3":
-				if (event.shiftKey) {
-					// TODO
-					// Store position and rotation
-					// setThirdPosition(position, rotation)
-					console.log("Store position three");
-					break;
-				}
-				// TODO
-				// Move to stored position
-				console.log("Fly to position three.");
-				break;
-			default:
-				break;
-		}
+				// Move to stored position camera moveTo()
+			}
+		});
 	};
 
 	const renderedPositionButtons = positionButtons.map(({ id, label }) => {
